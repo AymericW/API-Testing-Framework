860d3f058fdb19f6891841c3465242e7b3504dd1
Simon Pin <simonpin@hotmail.com>
2018-08-21 13:27:38 +0200

    update

--- pipelines/Runner.groovy
+++ pipelines/Runner.groovy
@@ -1,8 +1,7 @@
 #!groovy
 @Library('CIPaaS') import com.bnpparibasfortis.CIPaaServices
-
 node ('master'){
-
+//node ('IOS'){
     // Init CIPaaS Services
     def cipaas = new CIPaaServices(docker,steps,env)
     def cipaasDockerRegistryName = cipaas.getCIPaaSDockerRegistryName()
@@ -11,14 +10,10 @@ node ('master'){
     println "cipaasDockerRegistryName is ${cipaasDockerRegistryName}"
     println "centralNPMPublicGroupURL is ${centralNPMPublicGroupURL}"
     println "localNPMHostedRepositoryURL is ${localNPMHostedRepositoryURL}"
-
-
     // Node image used in this pipeline 
     def nodeImageName = "${cipaasDockerRegistryName}/cip/node:8.9.4-R1.7"
     def nodeImage = docker.image("${nodeImageName}")
-
     stage ('Start pipeline') {println "Start pipeline"}
-
     stage ('Checkout') {checkout scm}
     
     nodeImage.inside('-u root') {
@@ -27,14 +22,52 @@ node ('master'){
             steps.sh "npm install"
         }
         stage ('Run the script') {
-            steps.sh "./scripts/run.sh"
+            steps.sh "./scripts/run.sh 'features/ocal_getProductList.feature'"
         }
-        
         stage ('Upload the Report') {
-            steps.sh 'sh ./scripts/uploadscript.sh "CUSTOMERS" "Current_Release" "Become a Retail Customer (QAP1)" "API" "CUSTOMERS" "1.0" "API" "cucumberQA.json" "http://wpdm0006:8080"'
+            steps.sh 'sh ./scripts/uploadscript.sh "CUSTOMERS" "Current_Release" "getProductList (TEST)" "API" "Full Test" "API" "TEST" "cucumberQA.json" "http://wpdm0006.be.fortis.bank:8080/"'
         }
     }
-
     stage ('End pipeline') {println "End pipeline"}
-
 }
+
+// #!groovy
+// @Library('CIPaaS') import com.bnpparibasfortis.CIPaaServices
+
+// node ('master'){
+
+//     // Init CIPaaS Services
+//     def cipaas = new CIPaaServices(docker,steps,env)
+//     def cipaasDockerRegistryName = cipaas.getCIPaaSDockerRegistryName()
+//     def centralNPMPublicGroupURL = cipaas.getCentralNPMPublicGroupURL()
+//     def localNPMHostedRepositoryURL = cipaas.getLocalNPMHostedRepositoryURL()
+//     println "cipaasDockerRegistryName is ${cipaasDockerRegistryName}"
+//     println "centralNPMPublicGroupURL is ${centralNPMPublicGroupURL}"
+//     println "localNPMHostedRepositoryURL is ${localNPMHostedRepositoryURL}"
+
+
+//     // Node image used in this pipeline 
+//     def nodeImageName = "${cipaasDockerRegistryName}/cip/node:8.9.4-R1.7"
+//     def nodeImage = docker.image("${nodeImageName}")
+
+//     stage ('Start pipeline') {println "Start pipeline"}
+
+//     stage ('Checkout') {checkout scm}
+    
+//     nodeImage.inside('-u root') {
+//         stage ('Install NPM local dependencies') {
+//             steps.sh "npm config set registry ${centralNPMPublicGroupURL}"
+//             steps.sh "npm install"
+//         }
+//         stage ('Run the script') {
+//             steps.sh "./scripts/run.sh"
+//         }
+        
+//         stage ('Upload the Report') {
+//             steps.sh 'sh ./scripts/uploadscript.sh "CUSTOMERS" "Current_Release" "Become a Retail Customer (QAP1)" "API" "CUSTOMERS" "1.0" "API" "cucumberQA.json" "http://wpdm0006:8080"'
+//         }
+//     }
+
+//     stage ('End pipeline') {println "End pipeline"}
+
+// }
