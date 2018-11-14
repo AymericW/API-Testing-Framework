7ab14a2f771fb0e9acc3394e3e5b4032dedf1832
G27663 <simon.pin@bnpparibasfortis.com>
2018-11-14 11:23:46 +0100

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,10 +1,10 @@
 Feature=$1
 kinit -k -t g27663.keytab G27663@INT.SYS.SHARED.FORTIS
 unset HTTP_PROXY
-npm config set registry http://cipcentral-prod.be.net.intra/nexus/repository/BNPPF_NPM/
+npm config set registry http://wpdm0006:8081/nexus/content/groups/npm-public-and-private/
 npm install
-export HTTP_PROXY="http://nwbcproxy.res.sys.shared.fortis:8080"
-npm start features/$Feature.feature -f json:cucumberQA.json || true
+export HTTP_PROXY="http://cipcentral-prod.be.net.intra/nexus/repository/BNPPF_NPM"
+node_modules/cucumber/bin/cucumber.js features/$Feature.feature -f json:$Feature.json || true
 
 
 # unset HTTP_PROXY  
