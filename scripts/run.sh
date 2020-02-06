a1ca33777976d77cf5459ff754f0830fd8c4334e
WILMOTTE Aymeric <G47594@INTNP.SYSNP.SHARED.FORTIS>
2020-02-06 14:46:11 +0100

    added custom plugin

--- scripts/run.sh
+++ scripts/run.sh
@@ -11,4 +11,4 @@ npm start features/$Feature.feature -- --format json:$Feature.json || true
 # npm config set registry http://cipcentral-prod.be.net.intra/nexus/repository/BNPPF_NPM/  
 # npm install  
 # export HTTP_PROXY="http://nwbcproxy.res.sys.shared.fortis:8080"  
-# node_modules/cucumber/bin/cucumber.js -f json:cucumberQA.json || true
+# ./node_modules/cucumber/bin/cucumber.js -f json:cucumberQA.json || true
