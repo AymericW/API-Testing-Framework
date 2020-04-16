598631fadefb368db36d5d80c8c7e49343fe2226
WILMOTTE Aymeric <G47594@INTNP.SYSNP.SHARED.FORTIS>
2020-04-16 15:04:36 +0200

    refactor of code

--- features/contact_management.feature
+++ features/contact_management.feature
@@ -2,6 +2,6 @@
 Feature: As a customer i want to manage my contact points
 
    Scenario:
-   Given I am logged in as "15923532"
+   Given I am logged with smid "1180546302" and "67030417188221005" as cardnumber
    When I retrieve my contactpoints
    Then status code is "200"
--- features/step_definitions/contact_management_steps.js
+++ features/step_definitions/contact_management_steps.js
@@ -1,36 +1,20 @@
 const { Given, When, Then } = require("cucumber");
-var querystring = require('querystring');
+const assert = require('chai').assert;
+const querystring = require('querystring');
 const api = require('../../util/api');
-const root_url = "https://p1.easybanking.qabnpparibasfortis.be"
+const root_url = "https://p1.easybanking.qabnpparibasfortis.be";
 const Shell = require('node-powershell');
-const distributorId = '52FB001'
+
+
+const distributorId = '52FB001';
 let ucrToken;
 let ucrTokenFinal;
 let errorlog;
-var seea_server_cookies;
-
-
-
-const user = {
-    smid: "1180546302",
-    cardnumber: "67030417188221005",
-    name: 'Demo User'
-}
-var randHex = function(len) {
-    var maxlen = 8,
-        min = Math.pow(16, Math.min(len, maxlen) - 1)
-    max = Math.pow(16, Math.min(len, maxlen)) - 1,
-        n = Math.floor(Math.random() * (max - min + 1)) + min,
-        r = n.toString(16);
-    while (r.length < len) {
-        r = r + randHex(len - maxlen);
-    }
-    return r;
-};
-
-//const csrf = randHex(128);
-const csrf = '2e9312a346129e623ca0c830b874fcd3e25a8a0c1919f2d03414b7a13c5d9e65f447255cb9b2b69d485e13066c14cd0cf9e8bd8777be028ae468ffece305bef5627ce76f8d4c68d6a70880eae33b41e6407ab1c14f48830e50369b607042bfc8c9d0a6c601606b81545f3cb1c32818338924b4c1c9c8a27e87bba7140555fca2';
+let seea_server_cookies;
 let agreementId;
+const csrf = '2e9312a346129e623ca0c830b874fcd3e25a8a0c1919f2d03414b7a13c5d9e65f447255cb9b2b69d485e13066c14cd0cf9e8bd8777be028ae468ffece305bef5627ce76f8d4c68d6a70880eae33b41e6407ab1c14f48830e50369b607042bfc8c9d0a6c601606b81545f3cb1c32818338924b4c1c9c8a27e87bba7140555fca2';
+let responseBody;
+let responseStatusCode;
 
 const headers = {
     'CSRF': csrf,
@@ -39,15 +23,11 @@ const headers = {
     'Content-Type': 'application/json'
 }
 
-function processcallback(response) {
-    console.log(response.body);
-}
 
-Given('I am logged in as {string}', function(string) {
-    
-    api.post(root_url + '/EBIA-pr01/rpc/identAuth/retrieveLoginProfiles', { distributorId }, headers)
+const login = (smid, cardNumber, callback) => {
+    return api.post(root_url + '/EBIA-pr01/rpc/identAuth/retrieveLoginProfiles', { distributorId }, headers)
         .then((response) => {
-            
+
             response.headers['set-cookie'].forEach(header => {
                 headers.Cookie += header + ';'
 
@@ -56,8 +36,8 @@ Given('I am logged in as {string}', function(string) {
             return api.post(root_url + '/EBIA-pr01/rpc/identAuth/initiateLoginTransaction', {
                 saveAlias: '0',
                 distributorId,
-                smid: user.smid,
-                authenticationFactorId: user.cardnumber,
+                smid: smid,
+                authenticationFactorId: cardNumber,
                 minimumDacLevel: '5',
                 language: 'EN'
             }, headers)
@@ -71,7 +51,7 @@ Given('I am logged in as {string}', function(string) {
 
             return api.post(root_url + '/EBIA-pr01/rpc/identAuth/getLoginMeans', {
                 distributorId,
-                smid: user.smid,
+                smid: smid,
                 minimumDacLevel: '5'
             }, headers)
 
@@ -83,7 +63,7 @@ Given('I am logged in as {string}', function(string) {
 
             return api.post(root_url + '/EBIA-pr01/rpc/identAuth/executeLoginTransaction', {
                 distributorId,
-                smid: user.smid,
+                smid: smid,
                 requestedMeanId,
                 agreementId
             }, headers)
@@ -91,7 +71,7 @@ Given('I am logged in as {string}', function(string) {
         }).then((response) => {
             response.headers['set-cookie'].forEach(header => {
                 headers.Cookie += header + ';'
-          
+
             });
             const ps = new Shell({
                 executionPolicy: 'Bypass',
@@ -101,7 +81,7 @@ Given('I am logged in as {string}', function(string) {
             ps.addCommand('../../bypass.ps1');
 
 
-            ps.invoke().then((output) => {
+            return ps.invoke().then((output) => {
                 console.log(output)
                 ucrToken = JSON.parse(output)
                 errorlog = ucrToken.errorArea;
@@ -114,11 +94,11 @@ Given('I am logged in as {string}', function(string) {
                 console.log(ucrTokenFinal);
                 console.log(errorlog);
                 ps.dispose();
-                
+
                 seea_server_cookies = headers.Cookie;
-               
+
                 return api.post(root_url + '/EBIA-pr01/rpc/identAuth/checkLoginResult', {
-                    smid: user.smid,
+                    smid: smid,
                     distributorId,
                     ucr: {
                         signature: ucrTokenFinal
@@ -129,29 +109,28 @@ Given('I am logged in as {string}', function(string) {
                 console.log(response.body);
                 response.headers['set-cookie'].forEach(header => {
                     headers.Cookie += header + ';'
-              
+
                 });
-          
+
                 var auth = "";
-                 auth += "<DIST_ID>52FB001</DIST_ID>";
-                 auth += "<MEAN_ID>UCRS</MEAN_ID>";
-                 auth += "<EAI_AUTH_TYPE>UCRS</EAI_AUTH_TYPE>";
-                 auth += "<EBANKING_USER_ID><SMID>" + user.smid + "</SMID></EBANKING_USER_ID>";
-                 auth += "<EBANKING_USER_AUTHENTICITY_VALIDATION>";
-                 auth += "<AUTHENTICATION_MEAN_ID>08</AUTHENTICATION_MEAN_ID>";
-                 auth += "</EBANKING_USER_AUTHENTICITY_VALIDATION>";
+                auth += "<DIST_ID>52FB001</DIST_ID>";
+                auth += "<MEAN_ID>UCRS</MEAN_ID>";
+                auth += "<EAI_AUTH_TYPE>UCRS</EAI_AUTH_TYPE>";
+                auth += "<EBANKING_USER_ID><SMID>" + smid + "</SMID></EBANKING_USER_ID>";
+                auth += "<EBANKING_USER_AUTHENTICITY_VALIDATION>";
+                auth += "<AUTHENTICATION_MEAN_ID>08</AUTHENTICATION_MEAN_ID>";
+                auth += "</EBANKING_USER_AUTHENTICITY_VALIDATION>";
 
                 var encoded = encodeURIComponent(auth);
                 var auth_string = "AUTH=" + encoded;
-                var formData = querystring.stringify(auth_string); 
-                return api.postForm(root_url + '/SEEA-pa01/SEEAServer', 
-                     auth_string,
-                 {
-                    'CSRF': csrf,
-                    'Cookie': seea_server_cookies,
-                    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36',
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                })
+                var formData = querystring.stringify(auth_string);
+                return api.postForm(root_url + '/SEEA-pa01/SEEAServer',
+                    auth_string, {
+                        'CSRF': csrf,
+                        'Cookie': seea_server_cookies,
+                        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36',
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                    })
 
 
             }).then((response) => {
@@ -163,43 +142,41 @@ Given('I am logged in as {string}', function(string) {
                 console.log(response.statusCode);
                 console.log(response.headers['location']);
                 var locationSeeaServer = response.headers['location'];
-                return api.get( locationSeeaServer, headers);
-
-                }).then((response) => {
-                response.headers['set-cookie'].forEach(header => {
-                    headers.Cookie += header + ';'
-                console.log("SEEA server");
-                console.log(response.statusCode);
-                console.log(response.body);
-                });
-               
-                return api.post("https://p1.easybanking.qabnpparibasfortis.be/OCPL-pr01/rpc/consentData/getContactPointList", {}, headers);
-
+                return api.get(locationSeeaServer, headers);
 
             }).then((response) => {
-                console.log("I AM HERE");
-                console.log(response.body);
+                response.headers['set-cookie'].forEach(header => {
+                    headers.Cookie += header + ';'
+                    console.log("SEEA server");
+                    console.log(response.statusCode);
+                    console.log(response.body);
+                });
+                callback();
             })
 
 
-        }).then((response) => {
+        })
+
+}
 
 
-        });
-        
-
+Given('I am logged with smid {string} and {string} as cardnumber', function(string, string2, callback) {
+    login(string, string2, callback);
 });
 
 
 When('I retrieve my contactpoints', function(callback) {
-    // Write code here that turns the phrase above into concrete actions
-
+    api.post("https://p1.easybanking.qabnpparibasfortis.be/OCPL-pr01/rpc/consentManagement/getConsentList", {}, headers)
+        .then((response) => {
+            responseBody = response.body;
+            responseStatusCode = response.statusCode;
+            callback();
+        })
 });
 
 
 
-Then('status code is {string}', function(status, callback) {
+Then('status code is {string}', function(status) {
     // Write code here that turns the phrase above into concrete actions
-    //console.log(callback);
-    //callback.then(processcallback);
+    assert.equal(responseStatusCode, status);
 });
