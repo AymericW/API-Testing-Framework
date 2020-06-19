59ffb3e2a8049baefdd979aafad7fbdf1c9c3d66
Nicolas Mine <nicolas.mine@bnpparibasfortis.com>
2020-06-19 12:26:45 +0200

    Update script.sh

--- scripts/script.sh
+++ scripts/script.sh
@@ -4,6 +4,6 @@ curl -vk --negotiate --user : --cookie-jar cookies.txt --location https://i-net8
 #get ucr
 response= $(curl -L --cookie cookies.txt --negotiate --user : -vk -H 'CSRF: 7J2vNthZH8qrMarOBTirxJZT9IT0aTeKEftAQvHT1Q0zzwK5V8tzobkb6vxoCch0ixLknFdv6qkmUrGrdLu0imsAuTamQBiwAEKBCPWX15oPhTI7Ag8w17OIPNul6phI' -H 'Cookie: CSRF=7J2vNthZH8qrMarOBTirxJZT9IT0aTeKEftAQvHT1Q0zzwK5V8tzobkb6vxoCch0ixLknFdv6qkmUrGrdLu0imsAuTamQBiwAEKBCPWX15oPhTI7Ag8w17OIPNul6phI' -H "Content-type: application/json" -d '{"clientNumber":"1180546302","callMode":"01","previousWindowFlag":"0"}' 'https://i-net800-qa.be.fortis.bank/EBPGJ01/BE_FORTIS_EBPG-pr01-war/rpc/ucr/CalculateOTP')
 
-logText = "Token : "
+logText="Token : "
 echo $logText $response
 echo $logText > token.txt
