f2e254297109ac4d0f5a0aec15880bc1e31fbbfd
G47594 <aymeric.wilmotte@bnpparibasfortis.com>
2020-08-18 13:49:42 +0200

    maybe pipeline fix ???

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,7 +2,6 @@ Feature=$1
 token=$2
 kinit -k -t G47594.keytab G47594@INT.SYS.SHARED.FORTIS
 unset HTTP_PROXY
-npm install
 export HTTP_PROXY="http://nwbcproxy.res.sys.shared.fortis:8080"
 node ./node_modules/cucumber/bin/cucumber-js features/$Feature.feature -f json:reports/result.json || true
 
